5853aa4da460a3689d1be574364be52af091f35f
Piyush Prajapati <piyushoa2004@gmail.com>
2024-03-29 17:00:57 +0530

    minor change in register response

--- app/src/main/java/com/example/trackerapp/domain/model/RegisterResponse.kt
+++ app/src/main/java/com/example/trackerapp/domain/model/RegisterResponse.kt
@@ -1,13 +1,9 @@
 package com.example.trackerapp.domain.model
 
-import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
 
-@Serializable(with = RegisterResponseSerializer::class)
+@Serializable
 data class RegisterResponse(
     @SerialName("success")
     val success: Boolean,
@@ -25,26 +21,26 @@ data class Category(
     val id: String
 )
 
-@Serializable
-@SerialName("RegisterResponse")
-private data class RegisterResponseSurrogate(
-    val success: Boolean,
-    val message: String,
-    val id: String?
-)
+//@Serializable
+//@SerialName("RegisterResponse")
+//private data class RegisterResponseSurrogate(
+//    val success: Boolean,
+//    val message: String,
+//    val id: String?
+//)
 
-object RegisterResponseSerializer : KSerializer<RegisterResponse> {
-    override val descriptor: SerialDescriptor
-        get() = RegisterResponseSurrogate.serializer().descriptor
-
-    override fun deserialize(decoder: Decoder): RegisterResponse {
-        val surrogate = decoder.decodeSerializableValue(RegisterResponseSurrogate.serializer())
-        return RegisterResponse(surrogate.success, surrogate.message,
-            surrogate.id?.let { Category(it) })
-    }
-
-    override fun serialize(encoder: Encoder, value: RegisterResponse) {
-        val surrogate = RegisterResponseSurrogate(value.success, value.message, value.category?.id)
-        encoder.encodeSerializableValue(RegisterResponseSurrogate.serializer(), surrogate)
-    }
-}
+//object RegisterResponseSerializer : KSerializer<RegisterResponse> {
+//    override val descriptor: SerialDescriptor
+//        get() = RegisterResponseSurrogate.serializer().descriptor
+//
+//    override fun deserialize(decoder: Decoder): RegisterResponse {
+//        val surrogate = decoder.decodeSerializableValue(RegisterResponseSurrogate.serializer())
+//        return RegisterResponse(surrogate.success, surrogate.message,
+//            surrogate.id?.let { Category(it) })
+//    }
+//
+//    override fun serialize(encoder: Encoder, value: RegisterResponse) {
+//        val surrogate = RegisterResponseSurrogate(value.success, value.message, value.category?.id)
+//        encoder.encodeSerializableValue(RegisterResponseSurrogate.serializer(), surrogate)
+//    }
+//}
